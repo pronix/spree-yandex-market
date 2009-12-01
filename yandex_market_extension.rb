5a5f3db4a566a2563e132c17b78f41ee7d5f14eb
Maxim <parallel588@gmail.com>
2009-12-01 20:16:11 +0500

    Подключен гем nokogiri, и включение таб Яндекс.Маркет в админке

--- yandex_market_extension.rb
+++ yandex_market_extension.rb
@@ -3,14 +3,14 @@
 
 class YandexMarketExtension < Spree::Extension
   version "1.0"
-  description "Describe your extension here"
+  description "Export products to Yandex.Market"
   url "http://yourwebsite.com/yandex_market"
 
   # Please use yandex_market/config/routes.rb instead for extension routes.
 
-  # def self.require_gems(config)
-  #   config.gem "gemname-goes-here", :version => '1.2.3'
-  # end
+  def self.require_gems(config)
+    config.gem "nokogiri"
+  end
   
   def activate
 
@@ -19,16 +19,17 @@ class YandexMarketExtension < Spree::Extension
     # app/controllers/admin/yourextension_controller
     # and that you mapped your admin in config/routes
 
-    #Admin::BaseController.class_eval do
-    #  before_filter :add_yandex_market_tab
-    #
-    #  def add_yandex_market_tab
+    Admin::BaseController.class_eval do
+      before_filter :add_yandex_market_tab
+
+      def add_yandex_market_tab
     #    # add_extension_admin_tab takes an array containing the same arguments expected
     #    # by the tab helper method:
     #    #    :yandex_market, { :label => "YandexMarketExtension", :route => "/some/non/standard/route" }
-    #    add_extension_admin_tab :yandex_market
-    #  end
-    #end
+        # "/admin/yandex_markets" admin_yandex_markets_path
+       add_extension_admin_tab :yandex_market, { :label => "yandex_market", :route => "admin_yandex_markets" }
+      end
+    end
 
     # make your helper avaliable in all views
     # Spree::BaseController.class_eval do
