fd1105f5a9c0dccd4122ffc68b78090bc7d979ac
Maxim <parallel588@gmail.com>
2009-12-02 23:59:07 +0500

    ДОбавил Настройку какой товар выгружать

--- app/models/yandex_market.rb
+++ app/models/yandex_market.rb
@@ -2,7 +2,6 @@
 class YandexMarket < Configuration
   preference :category,     :string
   preference :currency,     :string
-  preference :only_on_hand, :boolean, :default => false
-  preference :all_active,   :boolean, :default => true
+  preference :wares,        :string, :default => "active"
   
 end
