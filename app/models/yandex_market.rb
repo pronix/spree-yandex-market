ae7f57f5c7459163892098c280240c79fb9b16ee
Maxim <parallel588@gmail.com>
2009-12-04 00:04:09 +0500

    Добавлены свойства на описание книг, музыки и билетов на мероприятие

--- app/models/yandex_market.rb
+++ app/models/yandex_market.rb
@@ -19,4 +19,39 @@ class YandexMarket < Configuration
   preference :manufacturer_warranty, :string, :default => "manufacturer_warranty" # есть официальная гарантию производителя.
   preference :wares_type,      :string, :default => "wares_type"   # Тип Товара
   
+  # wares property Книги и АудиоКниги
+  preference :author, :string            # Автор книги
+  preference :publisher, :string         # Издательство
+  preference :series, :string            # Серия
+  preference :year, :string              # Год издания
+  preference :isbn, :string              # Код книги, если их несколько, то указываются через запятую.
+  preference :volume, :string            # Количество томов.
+  preference :part, :string              # Номер тома.
+  preference :language, :string          # Язык произведения.
+  preference :binding, :string           # Переплет.
+  preference :page_extent, :string       # Количествово страниц в книге, должно быть целым положиельным числом.
+  preference :performed_by, :string      # Исполнитель. Если их несколько, перечисляются через запятую
+  preference :storage, :string           # Носитель, на котором поставляется аудиокнига.
+  preference :format, :string            # Формат аудиокниги.
+  preference :recording_length , :string # Время звучания задается в формате mm.ss (минуты.секунды).
+  
+  # wares property Музыка и Видео
+  preference :artist , :string          # Исполнитель
+  preference :title , :string           # Наименование 
+  preference :music_video_year, :string # Год
+  preference :media , :string           # Носитель
+  preference :starring , :string        # Актеры
+  preference :director , :string        # Режиссер
+  preference :orginal_name , :string     # Оригинальное наименовани
+  preference :video_country, :string    # Страна
+  
+  # wares property Билеты
+  preference :place, :string         # Место мероприятия
+  preference :hall, :string          # Зал
+  preference :hall_url_plan, :string # Ссылка на картинку версии зала
+  preference :event_date, :string    # Дата и время сеанса. Указываются в формате ISO 8601: YYYY-MM-DDThh:mm
+  preference :is_premiere, :string   # Признак премьерности мероприятия
+  preference :is_kids, :string       # Признак детского мероприятия
+  
 end
+
