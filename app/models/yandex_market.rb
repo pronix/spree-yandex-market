e5563402236022597238353f4f1e4073745294e5
Maxim <parallel588@gmail.com>
2009-12-03 13:20:31 +0500

    В настройка добавил кол-во файлов экспорта которое должно храниться

--- app/models/yandex_market.rb
+++ app/models/yandex_market.rb
@@ -1,7 +1,8 @@
 # class YandexMarket < ActiveRecord::Base
 class YandexMarket < Configuration
-  preference :category,     :string
-  preference :currency,     :string
-  preference :wares,        :string, :default => "active"
-  
+  preference :category,        :string
+  preference :currency,        :string
+  preference :wares,           :string,  :default => "active"
+  preference :number_of_files, :integer, :default => 5
+
 end
