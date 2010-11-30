2e4f8f30cf8af7f03c7b2b902bef6c9d7be58fb1
krick <antipin.konstantin@googlemail.com>
2010-11-30 22:39:49 +0300

    Решена возникшая проблема с product.master = nil

--- lib/export/torg_mail_ru_exporter.rb
+++ lib/export/torg_mail_ru_exporter.rb
@@ -51,7 +51,7 @@ module Export
               @categories && @categories.each do |cat|
                 products = @config.preferred_wares == "on_hand" ? cat.products.active.on_hand : cat.products.active      
                 products && products.each do |product|
-                  offer(xml,product, cat) if product.price > 0
+                  offer(xml,product, cat) if (product and product.master and product.master.price > 0)
                 end
               end          
             }
