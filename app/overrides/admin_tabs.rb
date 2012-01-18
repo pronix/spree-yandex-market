ee5ece54e34301d94e1645274b8a0882d4e1d4dd
Sergey Korotkov <sergey.korotkov@me.com>
2012-01-18 17:30:32 +0400

    switch to deface

---
+++ app/overrides/admin_tabs.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "layouts/admin",
+                     :name => "converted_admin_tabs",
+                     :insert_after => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     :text => "<%=  tab(:yandex_market, { :route => \"admin_yandex_market_settings\" })  %>",
+                     :disabled => false)
--- lib/spree_yandex_market_hooks.rb
+++
@@ -1,5 +0,0 @@
-class SpreeYandexMarketHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_tabs do
-    %(<%=  tab(:yandex_market, { :route => "admin_yandex_market_settings" })  %>)
-  end
-end
