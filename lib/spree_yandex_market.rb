ab1b2d1f1fcb9ccc9a0380810f946cc16f5cca89
Sergey Korotkov <sergey.korotkov@me.com>
2012-01-18 17:36:57 +0400

    loading overrides

--- lib/spree_yandex_market.rb
+++ lib/spree_yandex_market.rb
@@ -8,15 +8,20 @@ module SpreeYandexMarket
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
+      # Load application's view overrides
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
 
-#    rake_tasks do
-#      load File.join(File.dirname(__FILE__), "tasks/yandex_market.rake")
-#    end
+    #    rake_tasks do
+    #      load File.join(File.dirname(__FILE__), "tasks/yandex_market.rake")
+    #    end
 
     config.to_prepare &method(:activate).to_proc
   end
-  
+
 end
