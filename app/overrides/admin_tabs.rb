48073da3ea90f0ad25b76213cd527596f76ea491
hazg <hazg@mail.ru>
2013-01-03 18:44:43 +0400

    icon

--- app/overrides/admin_tabs.rb
+++ app/overrides/admin_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "converted_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%=  tab(:yandex_market, :route => 'admin_yandex_market_settings' )  %>",
+                     :text => "<%=  tab(:yandex_market, :icon => 'icon-shopping-cart', :route => 'admin_yandex_market_settings' )  %>",
                      :disabled => false)
